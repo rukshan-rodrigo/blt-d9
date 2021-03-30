f1373c61d5a96411c5164ad5613b828b233ca90b
Stephen Rodrigo <s.rodrigo@accenture.com>
2021-03-30 19:40:17 +1100

    Removes build files for css.

--- docroot/themes/custom/zero/theme-settings.php
+++ docroot/themes/custom/zero/theme-settings.php
@@ -5,10 +5,12 @@
  * Provides theme settings for STARTERKIT.
  */
 
+use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Implements hook_form_system_theme_settings_alter().
  */
-function zero_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id = NULL) {
+function zero_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
   // General "alters" use a form id. Settings should not be set here. The only
   // thing useful about this is if you need to alter the form for the running
   // theme and *not* the theme setting.
